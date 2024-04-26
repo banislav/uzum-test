2944c1b85bfe991ac80372c67e91243443622492
banikrukov <danyalapin@yandex.ru>
2024-04-27 00:12:51 +0500

    feat: implement a primitive version of multiclass classifier

---
+++ model/model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class ModelV1(nn.Module):
+    def __init__(self, in_shape=3, out_shape=5, hidden_units=10):
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=3, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=out_shape),
+            nn.LogSoftmax(out_shape)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.classifer(x)
